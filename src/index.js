ea2abf46c5e5c15f8e9a8565b144203fcbf33007
Mashudu Luvhengo <mluvhengo25@gmail.com>
2024-05-06 19:50:23 +0200

    updated auth

--- src/index.js
+++ src/index.js
@@ -1,107 +1,26 @@
-// var map = L.map('map');
-// map.setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
-
-// navigator.geolocation.watchPosition(myLocation, error);
-
-
-// let marker, circle, zoomed; 
-
-// function myLocation(pos){
-
-//     const lat = pos.coords.latitude;
-//     const lng = pos.coords.longitude;
-//     const accuracy = pos.coords.accuracy;
-
-//    marker = L.marker([lat, lng]).addTo(map);
-//    circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-
-//    if(!zoomed){
-//         zoom = map.fitBounds(circle.getBounds());
-//    }
-   
-//   map.setView([lat,lng]);
-
-// // var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// // var circle = L.circle([51.508, -0.11], {
-// //     color: 'red',
-// //     fillColor: '#f03',
-// //     fillOpacity: 0.5,
-// //     radius: 500
-// // }).addTo(map);
-// }
-
-// function error(err){
-//     if(err.code === 1){
-//         alert("please allow geolocation access");
-//     }else{
-//         alert("cannot get current location");
-//     }
-// }
-// //TODO: fetch coordinates from backend
-
-// function fetchLocationData() {
-//     axios.get("https://localhost:8080/api/location/get-locations")
-//     .then(response => {
-//         response.data.forEach(function (location){
-//             L.marker([location.latitude, location.longitude]).addTo(map)
-//             .bindPopup(`Vehicle ID: ${location.vehicle.vehicleId}<br>Latitude: ${location.latitude}<br>Longitude: ${location.longitude}<br>Timestamp: ${location.timestamp}`);
-//         });
-//     })
-//     .catch(error => {
-//         console.error("error fetching location data", error);
-//     });
-// }
-
-// var map = L.map('map');
-// map.setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
-
-// function fetchLocationData() {
-//     // axios.get("http://localhost:8080/api/location/all-vehicle-locations")
-//     // .then(response => {
-//     //     response.data.forEach(function (location){
-//     //         L.marker([location.latitude, location.longitude]).addTo(map)
-//     //         .bindPopup(`Latitude: ${location.latitude}<br>Longitude: ${location.longitude}`);
-//     //     });
-//     // })
-//     // .catch(error => {
-//     //     console.error("error fetching location data", error);
-//     // });
-
-//     axios.get('http://localhost:8080/api/location/all-vehicle-locations')
-//   .then(function (response) {
-//     var coordinates = response.data;
-//     for (var i = 0; i < coordinates.length; i++) {
-//       if (coordinates[i].x && coordinates[i].y) {
-//         var marker = L.marker([coordinates[i].x, coordinates[i].y])
-//           .bindPopup("Device: " + coordinates[i].device_type + '<br>' + "Time: " + coordinates[i].datetime)
-//           .addTo(map);
-//       }
-//     }
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// }
-// Initialize the Leaflet map
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-// Add the OpenStreetMap tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+document.addEventListener('DOMContentLoaded', function(){
+    const loginForm = document.getElementById('loginForm');
+    if (window.location.pathname !== '/auth/login.html') {
+        window.location.href = '/auth/login.html';
+    } else {
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+    
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+    
+            axios.post("http://localhost:8080/api/login", { username, password })
+                .then((response) => {
+                    // Handle successful login
+                    console.log(response.data);
+                    window.location.href = '../index.html';
+                })
+                .catch((error) => {
+                    // Handle login error
+                    console.error(error);
+                });
+        });
+    }
 
 // fetchLocationData();
 function fetchVehicleLocations() {
@@ -124,8 +43,19 @@ function fetchVehicleLocations() {
     });
 }
 
+if(document.getElementById('map')){
+    // Initialize the Leaflet map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add the OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 fetchVehicleLocations();
-//TODO: add markers
+}
+// fetchVehicleLocations();
 
 //register vehicle function
 function registerVehicle(make,model,regNumber){
@@ -173,7 +103,9 @@ function handleRegisterSubmit(event){
     registerVehicle(make, model, regNumber);
 }
 
+
 let registerForm = document.getElementById("register-form");
 registerForm.addEventListener("submit", handleRegisterSubmit);
 
 fetchVehicleData();
+});
